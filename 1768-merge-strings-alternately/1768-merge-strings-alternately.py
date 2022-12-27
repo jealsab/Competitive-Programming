afc85471c663d9bcc33a2384062edf8f888feaf4
jeal <101905536+jealsab@users.noreply.github.com>
2022-12-27 15:36:12 +0300

    Time: 29 ms (48.54%), Space: 13.4 MB (64.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -7,19 +7,19 @@ class Solution(object):
         """
         i=0
         j=0
-        s=""
+        output=""
         while(i<len(word1)and j<len(word2)):
-            s+=word1[i]
+            output+=word1[i]
             i+=1
-            s+=word2[j]
+            output+=word2[j]
             j+=1
         while i<len(word1):
-            s+=word1[i]
+            output+=word1[i]
             i+=1
         while j<len(word2):
-             s+=word2[j]
+             output+=word2[j]
              j+=1
-        return s
+        return output
 
           
 
