81d359954b4b5b0c65cd93fa169d49d1d2a03ab1
jeal <101905536+jealsab@users.noreply.github.com>
2023-01-25 18:39:03 +0300

    Time: 1098 ms (49.87%), Space: 27.9 MB (17.27%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,32 +1,25 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-#         n=len(skill)/2
         n = len(skill)
-        s = 0
+        res = 0
         oneGroup = sum(skill) // (n / 2)
         count=Counter(skill)
         
         if oneGroup*(n/2)!=sum(skill):
-            # print("no")
             return -1
-    
             
         for i in range(n):
             
             x=int(oneGroup-skill[i])
-            print(x,skill[i],count[x],count[skill[i]])
-            # print(skill[i])
             if count[skill[i]]<=0:
                 continue
             count[skill[i]]-=1
             if x in count and count[x]>=0:
                 count[x]-=1
-                s+=x*skill[i]
+                res+=x*skill[i]
             else:
-                print(count[x])
-                print(count[skill[i]])
                 return -1
-        return s
+        return res
             
             
                                 
@@ -57,16 +50,3 @@ class Solution:
 
 
 
-
-
-             
-#     ans = 0
-#     count = collections.Counter(skill)
-
-#     for s, freq in count.items():
-#       requiredSkill = teamSkill - s
-#       if count[requiredSkill] != freq:
-#         return -1
-#       ans += s * requiredSkill * freq
-
-#     return ans // 2
