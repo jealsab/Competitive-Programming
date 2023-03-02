dd7a56bdc63b4c88eaf782174a892bca1e6ea018
jeal <101905536+jealsab@users.noreply.github.com>
2023-03-02 21:37:06 +0300

    Time: 1371 ms (22.53%), Space: 13.9 MB (59.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,10 +2,6 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # window_size= len(s1)-1
         c1=Counter(s1)
-        
-# "ab"
-# "eidbaooo"
-   # ij
         i=0
         j= len(s1)-1
         for i in range(len(s2)):
@@ -18,14 +14,6 @@ class Solution:
         return False
     
     
-    
-#                   res=[]
-#         count_s=Counter(s[:k-1])
-#         for i in range(len(s)-k+1):
-#             count_s[s[i+k-1]]+=1
-#             if count_p==count_s:
-#                 res.append(i)
-#             count_s[s[i]]-=1
         
         # "ab" {a:1,b:1}
         # "eidbaooo" {i:1,b:2} if c1==c2 return true else
