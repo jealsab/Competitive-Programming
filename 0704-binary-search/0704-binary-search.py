5a9439058304f7ea1cacac29f9bfa775db552bde
jeal <101905536+jealsab@users.noreply.github.com>
2023-03-15 21:43:50 +0300

    Time: 296 ms (7.83%), Space: 25 MB (14.93%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,24 +3,25 @@ class Solution:
         
         
         
-        def rec(low ,high,nums):
-            mid =(low +high)//2
-            if low > high:
-                return -1
-            if nums[mid] < target:
-                return rec(mid+1, high, nums)
-            elif nums[mid] > target:
-                return rec(low, mid-1, nums)
-            elif nums[mid] == target:
-                return mid
+        def rec(i,nums):
+                if i >= len(nums):
+                    return -1
+                if nums[i]==target:
+                    return i
+                else:
+                    return rec(i+1,nums)
+        return rec(0, nums)
+            # mid =(low +high)//2
+            # if low > high:
+            #     return -1
+            # if nums[mid] < target:
+            #     return rec(mid+1, high, nums)
+            # elif nums[mid] > target:
+            #     return rec(low, mid-1, nums)
+            # elif nums[mid] == target:
+            #     return mid
             
                
-        return rec(0,len(nums)-1,nums)
+        # return rec(0,len(nums)-1,nums)
     
-        #     if i >= len(nums):
-        #         return -1
-        #     if nums[i]==target:
-        #         return i
-        #     else:
-        #         return rec(i+1,nums)
-        # return rec(0, nums)
+           
