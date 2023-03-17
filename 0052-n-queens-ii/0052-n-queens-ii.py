44b843221e03b6e401039a0a7fa87353354f0e8a
jeal <101905536+jealsab@users.noreply.github.com>
2023-03-17 19:33:21 +0300

    Time: 57 ms (45.40%), Space: 14 MB (28.95%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,7 +1,9 @@
 class Solution:
     def __init__(self):
         self.result = 0
-        self.valid_queen_pos  = set()
+        self.vertical = set()
+        self.neg_diag = set()
+        self.pos_diag = set()
     
     def totalNQueens(self, n: int) -> int:
 
@@ -13,52 +15,27 @@ class Solution:
         
         #base case
         if row > n:
-            if len(self.valid_queen_pos) == n:
-                self.result += 1
+            self.result += 1
             return 
         
         for col in range(1, n+1):
             #if current position is valid, choose that position (pick and move)
-            if self.isValid((row, col), self.valid_queen_pos, n):
-
-                self.valid_queen_pos.add((row, col))
+            if self.isValid((row, col)):
+                self.vertical.add(col)
+                self.pos_diag.add(row+col)
+                self.neg_diag.add(row-col)
+                
                 self.backtrack(row + 1, n)
-                self.valid_queen_pos.remove((row, col))
+                
+                self.vertical.remove(col)
+                self.pos_diag.remove(row+col)
+                self.neg_diag.remove(row-col)
         
     
-    def isValid(self, pos, valid_queen_pos, n):
-        
+    def isValid(self, pos):
         row, col = pos
-
-        row_index = row
-        #check if postion is valid vertically
-        while row_index > 0:
-            if (row_index, col) in valid_queen_pos:
-                return False
-
-            row_index -= 1
-
         
-        row_index = row  
-        diag_col = col
-        #check if position is valid right diagnolally
-        while row_index > 0 and diag_col <= n:
-            if (row_index, diag_col) in valid_queen_pos:
-                return False
-
-            row_index -= 1
-            diag_col += 1
-
-
-        row_index = row  
-        diag_col = col
-        #check if position is valid left diagnolally
-        while row_index > 0 and diag_col > 0:
-            if (row_index, diag_col) in valid_queen_pos:
-                return False
-
-            row_index -= 1
-            diag_col -= 1
+        if col in self.vertical or (row + col) in self.pos_diag or (row-col) in self.neg_diag:
+            return False
         
-        #otherwise return true
         return True
