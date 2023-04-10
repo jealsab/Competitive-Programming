d8c4e100ee32723a898d0ee6567a5779e9328045
jeal <101905536+jealsab@users.noreply.github.com>
2023-04-10 17:52:06 +0300

    added palindrome-number.python

---
+++ palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        temp=x
+        rev=0
+        while x>0:
+            n=x%10
+            rev=(rev*10)+n
+            x/=10
+        if rev==temp:
+            return True
+        else:
+            return False
