3035bd3ef67348b212aa6595ce2781a06e1676b2
jeal <jealsabb@gmail.com>
2023-04-10 14:52:30 +0300

    Solution source_and_sink

--- Source_and_sink.py
+++ Source_and_sink.py
@@ -1,21 +1,28 @@
+from collections import defaultdict
 n = int(input())
-matrix = []
-source = []
-sink = []
-
+src_graph = defaultdict(list)
+dest_graph=defaultdict(list)
+src=[]  
+dest=set() 
+for i in range(n):
+    dest.add(i+1)
+        
 for i in range(n):
     row = list(map(int, input().split()))
-    matrix.append(row)
-for i in range(n):
-    if sum(matrix[i]) == 0:
-        sink.append(i + 1)
+    for j in range(len(row)):
+        src_graph[i].append(row[j])
+    if 0 in src_graph[i] and len(set(src_graph[i]))==1:
+        src.append(i+1)
+    for x in range(len(row)):
+        if row[x]==0:
+            pass
+        else:
+            if x+1 in dest:
+                dest.remove(x+1)
+final_dest= [len(dest)]
+final_dest.extend(dest)               
+src_res=[len(src)]
+src_res.extend(src)
+print(*final_dest)  
+print(*src_res)
 
-    col = 0
-    for j in range(n):
-        col += matrix[j][i]
-
-    if col == 0:
-        source.append(i + 1)
-  
-print(len(source), *source)
-print(len(sink), *sink)
