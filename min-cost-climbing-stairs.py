df5c132354a36c3adae8264850ef79291702f522
jeal <101905536+jealsab@users.noreply.github.com>
2023-05-28 11:46:49 +0300

    added min-cost-climbing-stairs.python

---
+++ min-cost-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(cost)
+        dp = [0] * (n + 1)
+        # dp[0] = 0 
+        # dp[1] = 0
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+
+        return dp[n]
