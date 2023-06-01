623fa193b05086f0a848614d5756cadc3a20b3f7
jeal <101905536+jealsab@users.noreply.github.com>
2023-06-01 23:43:09 +0300

    added house-robber-ii.python

--- house-robber-ii.py
+++ house-robber-ii.py
@@ -7,20 +7,24 @@ class Solution(object):
         if len(nums)<=3:
             return max(nums)
 
-        dp1 = [0 for _ in range (len(nums)-1)]
+        dp1 = [0 for _ in range (len(nums)+1)]
+        dp2= [0 for _ in range (len(nums)+1)]
 
-        dp1[0] = nums[0]
-        dp1[1] = nums[1]
-        dp1[2] = nums[2]+nums[0]
-        
-        for i in range(3, len(dp1)):
-            dp1[i] = nums[i]+ max(dp1[0:i-1])  
-        dp2 = [0 for _ in range (len(nums)-1)]
+        dp1[-2]= nums[-1]
+        dp2[0] = nums[0]
 
-        dp2[0] = nums[1]
-        dp2[1] = nums[2]
-        dp2[2] = nums[1]+nums[3]
-        
-        for i in range(3, len(dp2)):
-            dp2[i] = nums[i+1]+ max(dp2[0:i-1])  
-        return max(max(dp1),max(dp2))
+        for i in range(len(nums)-2,-1,-1):
+            if i!=0:
+                dp1[i]=max(dp1[i+1],nums[i]+dp1[i+2])
+            else:
+                dp1[i] =max(dp1[i+1],dp1[i+2])
+            # print(dp1)
+
+        nums[-1]=0
+        for i in range(len(nums)-2,-1,-1):
+            dp2[i]=max(dp2[i+1],nums[i]+dp2[i+2])
+            
+            # print(dp2)
+
+
+        return max(dp1[0],dp2[0])
