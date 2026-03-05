187dc4caadb9e968573ff5708e4ca18902b4b177
Jeal Redi <jealsabb@gmail.com>
2026-03-05 22:10:23 +0300

    add merging arrays

--- A_Merging_Arrays.py
+++ A_Merging_Arrays.py
@@ -1,22 +1,28 @@
-size_nums1, size_nums2= list(map(int,input().split()))
-nums1 = list(map(int,input().split()))
-nums2=list(map(int,input().split()))
-res = []
-i = 0
-j = 0
-while i < size_nums1 and j < size_nums2:
-    if nums1[i] < nums2[j]:
-        res.append(nums1[i])
-        i+=1    
+n, m = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+i = 0  
+j = 0  
+merged = []
+
+
+while i < n and j < m:
+    if arr1[i] <= arr2[j]:
+        merged.append(arr1[i])
+        i += 1
     else:
-        res.append(nums2[j])
-        j+=1
-        
-while i < size_nums1:
-    res.append(nums1[i])
+        merged.append(arr2[j])
+        j += 1
+
+
+while i < n:
+    merged.append(arr1[i])
     i += 1
-while j < size_nums2:
-    res.append(nums2[j])
+
+while j < m:
+    merged.append(arr2[j])
     j += 1
- 
-print(*res)
+
+
+print(*merged)
